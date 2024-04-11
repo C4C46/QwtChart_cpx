684beaa265577762fea0e2c3a089b0a37359cba1
cao.peixuan <cao.peixuan@hzleaper.com>
2024-04-11 16:52:58 +0800

    根据外部传入数据接收后进行曲线显示

--- ChartDemo/ChartUpdaterThread.cpp
+++ ChartDemo/ChartUpdaterThread.cpp
@@ -1,45 +1,30 @@
 ﻿#include "ChartUpdaterThread.h"
-#include <QTime>
+
 
 ChartUpdaterThread::ChartUpdaterThread(QObject *parent, const QStringList &curveNames)
 	: QThread(parent), curveNames(curveNames)
 {
 	qsrand(QTime::currentTime().msec());
+
+	// 设置TCP服务器
+	tcpServer = new QTcpServer(this); // 确保在使用tcpServer之前实例化它
+	if (!tcpServer->listen(QHostAddress::Any, 12345)) {
+		qDebug() << "Unable to start the server: " << tcpServer->errorString();
+	}
+	else {
+		connect(tcpServer, &QTcpServer::newConnection, this, &ChartUpdaterThread::acceptConnection);
+	}
 }
 ChartUpdaterThread::~ChartUpdaterThread() {
-
+	tcpServer->close();
 }
 
 void ChartUpdaterThread::run() {
-	double x = 0;
+	running = true;
 	while (running) {
-		qreal xIncrement = qrand() % 50 + 1;
-		x += xIncrement;
-
-		for (const QString &curveName : curveNames) {
-			qreal y = qrand() % 100;
-			emit updateChart(curveName, x, y);
-		}
-
-		QThread::sleep(1);
+		QThread::msleep(100); // 调整为合适的延迟
 	}
 }
-
-//void ChartUpdaterThread::run() {
-//    double x = 0;
-//    while (running) {
-//        // 生成一个小于1的随机增量，模拟1米内有多个点
-//        qreal xIncrement = (qrand() % 100) / 100.0; // 产生0到1之间的随机数
-//        x += xIncrement;
-//
-//        for (const QString &curveName : curveNames) {
-//            qreal y = qrand() % 1000; // y值现在表示毫米，范围0到999
-//            emit updateChart(curveName, x, y);
-//        }
-//
-//        QThread::msleep(100); // 使用msleep代替sleep，以便更频繁地更新数据
-//    }
-//}
 
 void ChartUpdaterThread::stopRunning() {
 
@@ -49,4 +34,38 @@ void ChartUpdaterThread::stopRunning() {
 void ChartUpdaterThread::updateCurveNames(const QStringList &newCurveNames) {
 	QMutexLocker locker(&mutex); // 确保线程安全
 	curveNames = newCurveNames;
+}
+
+
+void ChartUpdaterThread::acceptConnection() {
+	clientSocket = tcpServer->nextPendingConnection();
+	connect(clientSocket, &QTcpSocket::readyRead, this, &ChartUpdaterThread::readData);
+}
+
+//void ChartUpdaterThread::readData() {
+//	QDataStream in(clientSocket);
+//	in.setVersion(QDataStream::Qt_DefaultCompiledVersion);
+//
+//	QString curveName;
+//	double x, y;
+//
+//	while (!in.atEnd()) {
+//		in >> curveName >> x >> y;
+//		emit updateChart(curveName, x, y);
+//	}
+//}
+
+void ChartUpdaterThread::readData() {
+    if (!clientSocket) return;
+
+    QByteArray data = clientSocket->readAll(); // 读取所有数据
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject json = doc.object();
+
+    QString curveName = json["curveName"].toString(); // 获取曲线名称
+    double x = json["x"].toDouble();
+    double y = json["y"].toDouble();
+
+    // 根据曲线名称和数据触发信号
+    emit updateChart(curveName, x, y);
 }
